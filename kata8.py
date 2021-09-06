b46451b51a5337d7ef1d96aa3c774d3f001ba8f2
Xar0P <pietroricardocres@hotmail.com>
2021-09-06 14:39:32 -0300

    added

---
+++ kata8.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/550f22f4d758534c1100025a/train/python
+
+def dirReduc(arr):
+    dir = " ".join(arr)
+    dir2 = dir.replace("NORTH SOUTH",'').replace("SOUTH NORTH",'').replace("EAST WEST",'').replace("WEST EAST",'')
+    dir3 = dir2.split()
+    return dirReduc(dir3) if len(dir3) < len(arr) else dir3
+
+print(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))
